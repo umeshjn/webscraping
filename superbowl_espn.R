8222df823e9d38ab7a7c47b63fee3c286d2dcd95
Umesh Narayanappa <u.jnms08@gmail.com>
2017-06-25 14:09:22 -0500

    Fetching Super Bowl MVP data from ESPN

--- superbowl_espn.R
+++ superbowl_espn.R
@@ -12,3 +12,18 @@ head(data)
 
 ## Writing into csv
 write.csv(data, "superbowlwinners.csv", row.names = FALSE)
+
+
+## Reading the Html page
+webpage <- read_html("http://www.espn.com/nfl/superbowl/history/mvps")
+
+## Fetching data and cleaning
+data = webpage %>% html_nodes("table") %>% html_table(fill =TRUE) %>% .[[1]]
+names(data)  = data[2,]
+data = data[-c(1,2),]
+
+head(data)
+
+## Writing into csv
+write.csv(data, "superbowl_MVPs.csv", row.names = FALSE)
+
