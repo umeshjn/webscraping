09725adb70c6e39b2a5138e7e5228e90b59ded7e
Umesh Narayanappa <u.jnms08@gmail.com>
2017-06-25 14:07:30 -0500

    Fetching Super Bowl Winners Data from ESPN website

---
+++ superbowl_espn.R
@@ -0,0 +1,14 @@
+library(rvest)
+
+## Reading the Html page
+webpage <- read_html("http://www.espn.com/nfl/superbowl/history/winners")
+
+## Fetching data and cleaning
+data = webpage %>% html_nodes("table") %>% html_table(fill =TRUE) %>% .[[1]]
+names(data)  = data[2,]
+data = data[-c(1,2),]
+
+head(data)
+
+## Writing into csv
+write.csv(data, "superbowlwinners.csv", row.names = FALSE)
