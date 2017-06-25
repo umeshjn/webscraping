3475c6cfdb776d4a3953b2fcf1ddbd51b926ff85
Umesh Narayanappa <u.jnms08@gmail.com>
2017-06-25 14:14:51 -0500

    Fetching Superbowl TouchDown Leaders from ESPN

--- superbowl_espn.R
+++ superbowl_espn.R
@@ -27,3 +27,20 @@ head(data)
 ## Writing into csv
 write.csv(data, "superbowl_MVPs.csv", row.names = FALSE)
 
+
+
+## Reading the Html page
+webpage <- read_html("http://www.espn.com/nfl/history/leaders")
+
+## Fetching data and cleaning
+data = webpage %>% html_nodes("table") %>% html_table(fill =TRUE) %>% .[[1]]
+names(data)  = data[2,]
+data = data[-c(1,2),]
+
+head(data)
+
+## Writing into csv
+write.csv(data, "superbowl_MVPs.csv", row.names = FALSE)
+
+
+
